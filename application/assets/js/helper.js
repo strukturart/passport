738c969ca48ac571dab2771b0844d10416c2f18d
strukturart <print@strukturart.com>
2021-06-06 09:40:44 +0200

    break down in pure js

---
+++ application/assets/js/helper.js
@@ -0,0 +1,172 @@
+"use strict";
+
+function notify(param_title, param_text, param_silent, requireInteraction) {
+  var options = {
+    body: param_text,
+    silent: param_silent,
+    requireInteraction: requireInteraction,
+  };
+
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(param_title, options);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(param_title, options, action);
+
+        document.addEventListener("visibilitychange", function () {
+          if (document.visibilityState === "visible") {
+            // The tab has become visible so clear the now-stale Notification.
+            notification.close();
+
+            toaster("yes", 2000);
+          }
+        });
+      }
+    });
+  }
+}
+
+//bottom bar
+function bottom_bar(left, center, right) {
+  document.querySelector("div#bottom-bar div#button-left").textContent = left;
+  document.querySelector(
+    "div#bottom-bar div#button-center"
+  ).textContent = center;
+  document.querySelector("div#bottom-bar div#button-right").textContent = right;
+
+  if (left == "" && center == "" && right == "") {
+    document.querySelector("div#bottom-bar").style.display = "none";
+  } else {
+    document.querySelector("div#bottom-bar").style.display = "block";
+  }
+}
+
+//top bar
+function top_bar(left, center, right) {
+  document.querySelector("div#top-bar div.button-left").innerHTML = left;
+  document.querySelector("div#top-bar div.button-center").textContent = center;
+  document.querySelector("div#top-bar div.button-right").textContent = right;
+
+  if (left == "" && center == "" && right == "") {
+    document.querySelector("div#top-bar").style.display = "none";
+  } else {
+    document.querySelector("div#top-bar").style.display = "block";
+  }
+}
+
+//silent notification
+function toaster(text, time) {
+  document.querySelector("div#toast").innerHTML = text;
+  var elem = document.querySelector("div#toast");
+  var pos = -100;
+  var id = setInterval(down, 5);
+  var id2;
+
+  function down() {
+    if (pos == 0) {
+      clearInterval(id);
+      setTimeout(() => {
+        id2 = setInterval(up, 5);
+      }, time);
+    } else {
+      pos++;
+      elem.style.top = pos + "px";
+    }
+  }
+
+  function up() {
+    if (pos == -1000) {
+      clearInterval(id2);
+    } else {
+      pos--;
+      elem.style.top = pos + "px";
+    }
+  }
+}
+
+function share(url) {
+  var activity = new MozActivity({
+    name: "share",
+    data: {
+      type: "url",
+      url: url,
+    },
+  });
+
+  activity.onsuccess = function () {};
+
+  activity.onerror = function () {
+    console.log("The activity encounter en error: " + this.error);
+  };
+}
+
+//check if internet connection
+function check_iconnection() {
+  function updateOfflineStatus() {
+    toaster("Your Browser is offline", 15000);
+    return false;
+  }
+
+  window.addEventListener("offline", updateOfflineStatus);
+}
+
+//wake up screen
+function screenWakeLock(param1) {
+  if (param1 == "lock") {
+    lock = window.navigator.requestWakeLock("screen");
+
+    lock.onsuccess = function () {
+      toaster("screen-lock", 10000);
+    };
+
+    lock.onerror = function () {
+      alert("An error occurred: " + this.error.name);
+    };
+  }
+
+  if (param1 == "unlock") {
+    if (lock.topic == "screen") {
+      lock.unlock();
+    }
+  }
+}
+
+window.goodbye = function () {
+  document.getElementById("goodbye").style.display = "block";
+
+  if (localStorage.clickcount) {
+    localStorage.clickcount = Number(localStorage.clickcount) + 1;
+  } else {
+    localStorage.clickcount = 1;
+  }
+
+  if (localStorage.clickcount == 3) {
+    message();
+  } else {
+    document.getElementById("ciao").style.display = "block";
+    setTimeout(function () {
+      window.close();
+    }, 4000);
+  }
+
+  function message() {
+    document.getElementById("donation").style.display = "block";
+    setTimeout(function () {
+      localStorage.clickcount = 1;
+
+      window.close();
+    }, 6000);
+  }
+};
--- application/assets/js/script.js
+++ application/assets/js/script.js
@@ -1,213 +1,185 @@
 //set your source path
 var dir_name = "passport";
+let dir_exist = false;
 
 //Global Vars
 var pos_focus = 0;
-var debug = true;
 var window_status = "list";
 var items = [];
 k = -1;
 
-$(document).ready(function () {
-  function finder() {
-    //document.querySelector("div#items-list").length = 0;
-    var filelist = navigator.getDeviceStorages("sdcard");
-    for (var i = 0; i < filelist.length; i++) {
-      var cursor = filelist[i].enumerateEditable();
+function finder() {
+  var filelist = navigator.getDeviceStorages("sdcard");
+  for (var i = 0; i < filelist.length; i++) {
+    var cursor = filelist[i].enumerateEditable();
 
-      cursor.onsuccess = function () {
-        if (cursor.result) {
-          var file = cursor.result;
+    cursor.onsuccess = function () {
+      if (cursor.result) {
+        var file = cursor.result;
 
-          if (file.type.match("image/*")) {
-            fileURL = URL.createObjectURL(file);
+        if (file.type.match("image/*")) {
+          fileURL = URL.createObjectURL(file);
 
-            var str = file.name;
+          var str = file.name;
 
-            var dir = str.split("/");
-            var file_name = dir[dir.length - 1];
+          var dir = str.split("/");
+          var file_name = dir[dir.length - 1];
 
-            var file_path = "";
-            var file_path = str.split("/").slice(0, -1).join("/") + "/";
-            console.log(dir[2]);
-            if (dir[2] == dir_name) {
-              items.push([file_name, fileURL]);
-              k++;
-              $("ul#items-list").append(
-                '<li class="items"  tabindex="' +
-                  k +
-                  '"><div class="item-name">' +
-                  file_name +
-                  '</div><img src="' +
-                  fileURL +
-                  '"></li>'
-              );
-            } else {
-              document.getElementById("messages").style.display = "block";
-              document.getElementById("empty-message").style.diisplay = "block";
-            }
-          }
-          $("li[tabindex=0]").focus();
+          if (dir[2] == dir_name) {
+            dir_exist = true;
+            k++;
 
-          if (items.length == 0) {
-            document.getElementById("messages").style.display = "block";
-            document.getElementById("dir-not-exist").style.diisplay = "block";
-          }
+            let elm = document.createElement("LI");
+            elm.setAttribute("tabindex", k);
+            elm.classList.add("items");
 
-          if (items.length > 0) {
-            document.getElementById("messages").style.display = "none";
-            document.getElementById("dir-not-exist").style.diisplay = "none";
-          }
+            let div = document.createElement("DIV");
+            div.classList.add("item-name");
+            div.innerText = file_name;
 
-          // Once we found a file we check if there is other results
-          if (!this.done) {
-            // Then we move to the next result, which call the
-            // cursor success with the next file as result.
-            this.continue();
-          }
-          if (this.done) {
+            let img = document.createElement("img");
+            img.src = fileURL;
+            elm.appendChild(img);
+            elm.appendChild(div);
+
+            document.getElementById("items-list").appendChild(elm);
           }
         }
-      };
 
-      cursor.onerror = function () {};
-    }
-  }
-
-  finder();
-
-  function show_image() {
-    window_status = "image_view";
-    $("li.items").css("display", "none");
-    $(":focus").css("display", "block");
-    $(":focus img").css("display", "block");
-    $(":focus div.item-name").css("display", "none");
-  }
-
-  function show_image_list() {
-    if (window_status == "image_view") {
-      $("li.items div.item-name").css("display", "block");
-      $("li.items").css("display", "block");
-      $("img").css("display", "none");
-    }
-    window_status = "list";
-  }
-
-  ////////////////////////
-  //NAVIGATION
-  /////////////////////////
-
-  function nav(move) {
-    if (move == "+1") {
-      pos_focus++;
-
-      if (pos_focus <= items.length) {
-        $("div#finder")
-          .find("li[tabindex=" + pos_focus + "]")
-          .focus();
-      }
-
-      if (pos_focus == items.length) {
-        pos_focus = 0;
-        $("li[tabindex=0]").focus();
-      }
-    }
-
-    if (move == "-1") {
-      pos_focus--;
-      if (pos_focus >= 0) {
-        $("div#finder")
-          .find("li[tabindex=" + pos_focus + "]")
-          .focus();
-      }
-
-      if (pos_focus == -1) {
-        pos_focus = items.length - 1;
-
-        $("div#finder")
-          .find("li[tabindex=" + pos_focus + "]")
-          .focus();
-      }
-    }
-  }
-
-  var key_time;
-  var press_time = 0;
-  var longpress = false;
-  function func_interval() {
-    longpress = false;
-    press_time = 0;
-    key_time = setInterval(function () {
-      press_time++;
-
-      if (press_time > 2) {
-        longpress = true;
-      }
-      if (press_time < 2) {
-        longpress = false;
-      }
-    }, 1000);
-  }
-
-  //////////////////////////
-  ////KEYPAD TRIGGER////////////
-  /////////////////////////
-  function handleKeyDown(evt) {
-    switch (evt.key) {
-      case "Enter":
-        show_image();
-        break;
-
-      case "Backspace":
-        evt.preventDefault();
-
-        if (window_status == "list") {
-          window.close();
+        // Once we found a file we check if there is other results
+        if (!this.done) {
+          // Then we move to the next result, which call the
+          // cursor success with the next file as result.
+          this.continue();
         }
+      }
+      document.querySelectorAll("li.items")[0].focus();
+      items = document.querySelectorAll("li.items");
 
-        break;
+      if (dir_exist) {
+        document.getElementById("messages").style.display = "none";
+        document.getElementById("dir-not-exist").style.diisplay = "none";
+      }
+      if (!dir_exist) {
+        document.getElementById("messages").style.display = "block";
+        document.getElementById("dir-not-exist").style.diisplay = "block";
+      }
+    };
+
+    cursor.onerror = function () {
+      console.log("err");
+    };
+  }
+}
+
+finder();
+
+bottom_bar("qr", "", "");
+
+//to do
+//https://github.com/davidshimjs/qrcodejs
+
+function show_image() {
+  window_status = "image_view";
+  let a = document.querySelectorAll("li.items");
+  for (i = 0; i < a.length; i++) {
+    a[i].style.display = "none";
+    a[i].querySelectorAll(".item-name")[0].style.display = "none";
+  }
+  document.activeElement.style.display = "block";
+
+  document.activeElement.querySelectorAll("img")[0].style.display = "block";
+}
+
+function show_image_list() {
+  if (window_status == "image_view") {
+    let a = document.querySelectorAll("li.items");
+    for (i = 0; i < a.length; i++) {
+      a[i].style.display = "block";
+      a[i].querySelectorAll("div.item-name")[0].style.display = "block";
+      a[i].getElementsByTagName("IMG")[0].style.display = "none";
+    }
+  }
+  window_status = "list";
+}
+
+////////////////////////
+//NAVIGATION
+/////////////////////////
+
+function nav(move) {
+  if (move == "+1") {
+    pos_focus++;
+    if (pos_focus <= items.length) {
+      items[pos_focus].focus();
+    }
+
+    if (pos_focus >= items.length) {
+      pos_focus = 0;
+      items[0].focus();
     }
   }
 
-  function handleKeyUp(evt) {
-    clearInterval(key_time);
+  if (move == "-1") {
+    pos_focus--;
+    if (pos_focus >= 0) {
+      items[pos_focus].focus();
+    }
 
-    switch (evt.key) {
-      case "Enter":
-        break;
-
-      case "Backspace":
-        evt.preventDefault();
-        if (window_status == "list") {
-          window.close();
-        }
-
-        if (window_status == "image_view") show_image_list();
-
-        break;
-
-      case "ArrowDown":
-        nav("+1");
-        break;
-
-      case "ArrowUp":
-        nav("-1");
-        break;
-
-      case "ArrowRight":
-        nav("slide_right");
-        break;
-
-      case "ArrowLeft":
-        nav("slide_left");
-
-        break;
-
-      case "SoftLeft":
-        break;
+    if (pos_focus == -1) {
+      pos_focus = items.length - 1;
+      items[pos_focus].focus();
     }
   }
+}
 
-  document.addEventListener("keydown", handleKeyDown);
-  document.addEventListener("keyup", handleKeyUp);
-});
+//////////////////////////
+////KEYPAD TRIGGER////////////
+/////////////////////////
+function handleKeyDown(evt) {
+  switch (evt.key) {
+    case "Enter":
+      show_image();
+      break;
+
+    case "Backspace":
+      evt.preventDefault();
+
+      if (window_status == "list") {
+        window.close();
+      }
+
+      break;
+  }
+}
+
+function handleKeyUp(evt) {
+  switch (evt.key) {
+    case "Enter":
+      break;
+
+    case "Backspace":
+      evt.preventDefault();
+      if (window_status == "list") {
+        window.close();
+      }
+
+      if (window_status == "image_view") show_image_list();
+
+      break;
+
+    case "ArrowDown":
+      nav("+1");
+      break;
+
+    case "ArrowUp":
+      nav("-1");
+      break;
+
+    case "SoftLeft":
+      break;
+  }
+}
+
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
